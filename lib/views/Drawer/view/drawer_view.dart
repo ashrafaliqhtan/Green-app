670c62d6d82a8a328570ba484cab1f46d632ef26
Saadxf <vip.saadxf@gmail.com>
2024-04-17 10:22:24 +0300

    merge sultan3

--- lib/views/Drawer/view/drawer_view.dart
+++ lib/views/Drawer/view/drawer_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:green_saudi_app/extensions/screen_handler.dart';
 import 'package:green_saudi_app/utils/colors.dart';
@@ -15,96 +15,144 @@ class DrawerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: context.getWidth() * .7,
-      decoration: BoxDecoration(
-          color: green,
-          borderRadius: const BorderRadius.only(
-              topRight: Radius.circular(20), bottomRight: Radius.circular(20))),
-      padding: const EdgeInsets.only(top: 50, bottom: 70, left: 10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              const CircleAvatar(),
-              width16,
-              Text(
-                'Sultan Alotaibi',
-                style: TextStyle(
-                  color: pureWhite,
-                  fontWeight: FontWeight.bold,
+    return BlocProvider(
+      create: (context) => AuthBloc(),
+      child: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthLogoutSuccessState) {
+            context.getMessagesBar(msg: state.message, color: green);
+            context.push(view: const LoginView(), isPush: false);
+          } else if (state is AuthLogoutErrorState) {
+            context.getMessagesBar(msg: state.message, color: red);
+          }
+        },
+        builder: (context, state) {
+          return Container(
+            width: context.getWidth() * .7,
+            decoration: BoxDecoration(
+                color: green,
+                borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomRight: Radius.circular(20))),
+            padding: const EdgeInsets.only(top: 50, bottom: 70, left: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    const CircleAvatar(),
+                    width16,
+                    Text(
+                      'Sultan Alotaibi',
+                      style: TextStyle(
+                        color: pureWhite,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
                 ),
-              )
-            ],
-          ),
-          Column(
-            children: [
-              DrawerItem(
-                icon: Icons.person,
-                title: 'الحساب',
-                onTap: () {
-                  context.push(view: ProfileUser(), isPush: false);
-                },
-              ),
-              const DrawerItem(
-                icon: Icons.info,
-                title: 'من نحن',
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Divider(color: pureWhite,thickness: 0.2,height: 0.2,),
-              ),
-              const DrawerItem(
-                icon: Icons.eco,
-                title: 'Sustainability',
-              ),
-              const DrawerItem(
-                icon: Icons.nature,
-                title: 'Nature',
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Divider(color: pureWhite,thickness: 0.2,height: 0.2,),
-              ),
-            ],
-          ),
-          height20,
-          Row(
-            children: [
-              Icon(Icons.settings, color: pureWhite),
-              width8,
-              Text(
-                'الاعدادات',
-                style: TextStyle(color: pureWhite, fontWeight: FontWeight.bold),
-              ),
-              width8,
-              Container(width: 2, height: 20, color: pureWhite),
-              width8,
-              Text(
-                'تسجيل الخروج',
-                style: TextStyle(color: pureWhite, fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-          height20,
-          Row(
-            children: [
-              IconButton(
-                  onPressed: () {}, icon: Icon(Icons.facebook_outlined, color: pureWhite,
-                  )),
-              IconButton(
-                  onPressed: () {}, icon: Icon(FontAwesomeIcons.instagram,  color: pureWhite,
-                  )),
-              IconButton(
-                  onPressed: () {}, icon: Icon(FontAwesomeIcons.youtube,  color: pureWhite,
-                  )),
-              IconButton(
-                  onPressed: () {}, icon: Icon(FontAwesomeIcons.twitter,  color: pureWhite,
-                  )),
-            ],
-          )
-        ],
+                Column(
+                  children: [
+                    DrawerItem(
+                      icon: Icons.person,
+                      title: 'الحساب',
+                      onTap: () {
+                        context.push(view: ProfileUser(), isPush: false);
+                      },
+                    ),
+                    const DrawerItem(
+                      icon: Icons.info,
+                      title: 'من نحن',
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Divider(
+                        color: pureWhite,
+                        thickness: 0.2,
+                        height: 0.2,
+                      ),
+                    ),
+                    const DrawerItem(
+                      icon: Icons.eco,
+                      title: 'Sustainability',
+                    ),
+                    const DrawerItem(
+                      icon: Icons.nature,
+                      title: 'Nature',
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Divider(
+                        color: pureWhite,
+                        thickness: 0.2,
+                        height: 0.2,
+                      ),
+                    ),
+                  ],
+                ),
+                height20,
+                Row(
+                  children: [
+                    Icon(Icons.settings, color: pureWhite),
+                    width8,
+                    InkWell(
+                      onTap: () {
+                        context.push(view: const SettingsUser(), isPush: true);
+                      },
+                      child: Text(
+                        'الاعدادات',
+                        style: TextStyle(
+                            color: pureWhite, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    width8,
+                    Container(width: 2, height: 20, color: pureWhite),
+                    width8,
+                    InkWell(
+                      onTap: () {
+                        context.read<AuthBloc>().add(LogoutEvent());
+                      },
+                      child: Text(
+                        'تسجيل الخروج',
+                        style: TextStyle(
+                            color: pureWhite, fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
+                height20,
+                Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.facebook_outlined,
+                          color: pureWhite,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          FontAwesomeIcons.instagram,
+                          color: pureWhite,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          FontAwesomeIcons.youtube,
+                          color: pureWhite,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          FontAwesomeIcons.twitter,
+                          color: pureWhite,
+                        )),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
