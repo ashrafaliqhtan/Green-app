c80490cb5593fc3fe4679d770a5e5c8158a312be
ohob745 <110139139+ohob745@users.noreply.github.com>
2024-04-18 19:21:33 +0300

    upload image and get url v2

--- lib/views/profile/view/edit_profile_user.dart
+++ lib/views/profile/view/edit_profile_user.dart
@@ -4,7 +4,7 @@ import 'package:flutter_localization/flutter_localization.dart';
 import 'package:green_saudi_app/localistion/localistion.dart';
 import 'package:green_saudi_app/utils/colors.dart';
 import 'package:green_saudi_app/utils/spacing.dart';
-import 'package:green_saudi_app/views/profile/bloc/image_pic_bloc.dart';
+import 'package:green_saudi_app/image_picker/bloc/image_pic_bloc.dart';
 import 'package:green_saudi_app/views/profile/widget/custom_drop_menu.dart';
 
 class EditProfileUser extends StatelessWidget {
@@ -17,130 +17,148 @@ class EditProfileUser extends StatelessWidget {
 
     return BlocProvider(
       create: (context) => ImagePicBloc(),
-      child: Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-
-        appBar: AppBar(
-          actions:  [Text( AppLocale.editTitle.getString(context),style: TextStyle(color: pureWhite, fontSize: 25),)],
-          backgroundColor: green,
-          automaticallyImplyLeading: true,
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              InkWell(
-                onTap: () {
-                  BlocProvider.of<ImagePicBloc>(context).add(SelectImage());
-                },
-                child: SizedBox(
-                  height: 150,
-                  width: 150,
-                  child: BlocBuilder<ImagePicBloc, ImagePicState>(
+      child: BlocBuilder<ImagePicBloc, ImagePicState>(
+        builder: (context, state) {
+          return Scaffold(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            appBar: AppBar(
+              actions: [
+                Text(
+                  AppLocale.editTitle.getString(context),
+                  style: TextStyle(color: pureWhite, fontSize: 25),
+                )
+              ],
+              backgroundColor: green,
+              automaticallyImplyLeading: true,
+            ),
+            body: Center(
+              child: Column(
+                children: [
+                  BlocBuilder<ImagePicBloc, ImagePicState>(
                     builder: (context, state) {
-                      if (state is ImageState) {
-                        return CircleAvatar(
-                            radius: 100,
-                            backgroundImage: FileImage(state.fileImage1));
-                      } else {
-                        return Image.network(
-                            "https://image.movieglu.com/7772/GBR_007772h0.jpg");
-                      }
+                      return InkWell(
+                        onTap: () {
+                          BlocProvider.of<ImagePicBloc>(context)
+                              .add(SelectImage());
+                        },
+                        child: SizedBox(
+                          height: 150,
+                          width: 150,
+                          child: BlocBuilder<ImagePicBloc, ImagePicState>(
+                            builder: (context, state) {
+                              if (state is ImageState) {
+                                return CircleAvatar(
+                                    radius: 100,
+                                    backgroundImage:
+                                        FileImage(state.fileImage1));
+                              } else {
+                                return Image.network(
+                                    "https://image.movieglu.com/7772/GBR_007772h0.jpg");
+                              }
+                            },
+                          ),
+                        ),
+                      );
                     },
                   ),
-                ),
-              ),
-              height10,
-              const SizedBox(
-                width: 250,
-                child: DropMenu(),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 350,
-                  height: 82,
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(40)),
-                  child: Padding(
+                  height10,
+                  const SizedBox(
+                    width: 250,
+                    child: DropMenu(),
+                  ),
+                  Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.man_outlined,
-                          color: green,
-                          shadows: [Shadow(color: black, blurRadius: 10)],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(
-                                width: 250,
-                                height: 50,
-                                child: TextField(
-                                    controller: nameController,
-                                    decoration:  InputDecoration(
-                                      hintText:  AppLocale.userName.getString(context),
-                                    )),
+                    child: Container(
+                      width: 350,
+                      height: 82,
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(40)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.man_outlined,
+                              color: green,
+                              shadows: [Shadow(color: black, blurRadius: 10)],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    width: 250,
+                                    height: 50,
+                                    child: TextField(
+                                        controller: nameController,
+                                        decoration: InputDecoration(
+                                          hintText: AppLocale.userName
+                                              .getString(context),
+                                        )),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 350,
-                  height: 82,
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(40)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.man_outlined,
-                          color: green,
-                          shadows: [Shadow(color: black, blurRadius: 10)],
+                            )
+                          ],
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(
-                                width: 250,
-                                height: 50,
-                                child: TextField(
-                                    controller: phoneController,
-                                    decoration:  InputDecoration(
-                                      hintText:  AppLocale.phoneNumber.getString(context),
-                                    )),
-                              )
-                            ],
-                          ),
-                        )
-                      ],
+                      ),
                     ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 350,
+                      height: 82,
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(40)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.man_outlined,
+                              color: green,
+                              shadows: [Shadow(color: black, blurRadius: 10)],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    width: 250,
+                                    height: 50,
+                                    child: TextField(
+                                        controller: phoneController,
+                                        decoration: InputDecoration(
+                                          hintText: AppLocale.phoneNumber
+                                              .getString(context),
+                                        )),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: green,
+                    child: const Text("حفظ"),
+                  )
+                ],
               ),
-              Container(
-                color: green,
-                child: const Text("حفظ"),
-              )
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
